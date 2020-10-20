36a0d8afbeebe942dd467f8c9260a061a18828d0
Adeyemi Esther <estheradeyemi796@gmail.com>
2020-10-20 11:00:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 const navSlide=()=>{
     const burger = document.getElementById('btn');
-    const list=document.getElementById('move');
-    
-    burger.addEventListener('click', () => {
-        if(list.style.display === "block"){
-            list.style.display = "none"
-        }else{
-            list.style.display = "block"
-        }
-    })
+    const list=document.querySelector('.list');
     const toggle = document.getElementById('cancel');
     
-    toggle.addEventListener('click', () => {
-        if(list.style.display === "block"){
-            list.style.display = "none"
-        }else{
-            list.style.display = "block"
-        }
-    })
-    // burger.addEventListener('click',()=>{
-    //     list.classList.toggle('list');
-    // },false)
+    burger.addEventListener('click',()=>{
+        list.classList.toggle('please');
+    },false)
+
+    toggle.addEventListener('click',()=>{
+        list.classList.toggle('please');
+    },false)
 }
 navSlide();
+
+
+    // burger.addEventListener('click', () => {
+    //     if(list.style.display === "block"){
+    //         list.style.display = "none"
+    //     }else{
+    //         list.style.display = "block"
+    //     }
+    // })
+    // const toggle = document.getElementById('cancel');
+    
+    // toggle.addEventListener('click', () => {
+    //     if(list.style.display === "block"){
+    //         list.style.display = "none"
+    //     }else{
+    //         list.style.display = "block"
+    //     }
+    // })
+// function classToggle() {
+//     const burger = document.getElementById('btn');
+//     const lists=document.getElementById('move');  
+//     lists.forEach(list => list.classList.toggle('please'));
+//   }
+  
+// document.querySelector(burger).addEventListener('click', classToggle);
